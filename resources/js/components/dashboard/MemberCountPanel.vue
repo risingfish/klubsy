acc8a79fbec00c63c199dd10dddac0dbb52307ce
risingfish <risingfish@gmail.com>
2025-10-15 22:53:50 -0600

    Initial commit to get things versioned

---
+++ resources/js/components/dashboard/MemberCountPanel.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col space-x-2 p-2">
+        <h2 class="text-[2rem] font-semibold">Members: 45</h2>
+    </div>
+</template>
+
